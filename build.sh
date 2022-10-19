eecacd0cb6c3c15480e3a4d74b350268177d639b
Artem Makogon <artem.makogon.nsk@mail.ru>
2022-10-19 17:42:37 +0300

    workspace

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@ pdflatex main.tex -interaction=nonstopmode
 biber main 
 pdflatex main.tex -interaction=nonstopmode
 pdflatex main.tex -interaction=nonstopmode
-cp /Content/main.pdf /main.pdf
+cp /Content/main.pdf /github/workspace/main.pdf
 
-ls /
+ls /github/workspace
