7a011b947f8bc004e81502c4619df27beaebea46
Artem Makogon <artem.makogon.nsk@mail.ru>
2022-10-21 23:22:39 +0300

    workflow

--- build.sh
+++ build.sh
@@ -1,8 +1,46 @@
-cd /Content
-pdflatex main.tex -interaction=nonstopmode
-biber main 
-pdflatex main.tex -interaction=nonstopmode
-pdflatex main.tex -interaction=nonstopmode
-cp /Content/main.pdf /github/workspace/main.pdf
+cd Content
 
-ls /github/workspace
+
+function latex_build {
+    pdflatex -interaction=nonstopmode '\newcommand{\student}{'"$1"'}\newcommand{\group}{'"$2 $3"'}\input{main}'
+    biber main 
+    pdflatex -interaction=nonstopmode '\newcommand{\student}{'"$1"'}\newcommand{\group}{'"$2 $3"'}\input{main}'
+    pdflatex -interaction=nonstopmode '\newcommand{\student}{'"$1"'}\newcommand{\group}{'"$2 $3"'}\input{main}'
+    cp /Content/main.pdf /github/workspace/$4.pdf
+}
+
+declare -a cases
+names=(
+    "Макогон А.А."
+    "Хамитов Т.Н."
+    "Жукова Д.А."
+    "Петренко К.Е."
+    "Ершов И.П."
+    "Брусенин Д.А."
+    "Test"
+)
+groups=(
+    "206"
+    "206"
+    "208"
+    "206"
+    "208"
+    "208"
+    "404"
+)
+files=(
+    "Makogon"
+    "Khamitov"
+    "Zhukova"
+    "Petrenko"
+    "Erhsov"
+    "Brusenin"
+    "article"
+)
+
+
+for ((i = 0; i < ${#names[@]}; i+= 1)) do
+    latex_build ${names[$i]} ${groups[$i]} ${files[$i]} 
+done 
+
+# ls /github/workspace
--- publish.sh
+++ publish.sh
@@ -2,14 +2,8 @@
 
 ME='makogon2907'
 
-ls 
-pwd
 
-echo ""
-
-tree Content
-
-cp -r main.pdf _gh_pages/article.pdf
+cp -r *.pdf _gh_pages/
 
 function get_commit_message {
     echo "pdf updates" # TODO copy commit message from target commit
@@ -21,7 +15,7 @@ cd _gh_pages
 
 git config user.name "$ME"
 git config user.email "$ME@users.noreply.github.com"
-git add article.pdf
+git add .
 git commit -m "$commit_message"
 
 git push origin gh_pages
